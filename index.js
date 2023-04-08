9983be36f38bfa9651b5565e996095984f0850ee
Chavez Harris <codedbychavez@gmail>
2023-04-08 14:26:13 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const configcat = require("configcat-node");
+
+// Create configcat client
+
+function suggestMoviesToUser() {
+  // TODO: Implement
+}
+
+function greetUser() {
+  // TODO: Implement
+}
+
+function main() {
+
+}
